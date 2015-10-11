70a473d285e26a0546e5aa597a7a5d74058f9928
kidocelano <reg.accsharing@gmail.com>
2015-10-11 19:56:00 +0700

    pre release

--- source/src/org/jpstool/main/MainDriven.java
+++ source/src/org/jpstool/main/MainDriven.java
@@ -1,7 +1,14 @@
 package org.jpstool.main;
 
+import java.io.File;
+
 public class MainDriven {
 	public static void main(String[] args) {
 		//new FlashResultFrame().setVisible(true);
+		testing_loopRandom();
+	}
+	
+	private static void testing_loopRandom() {
+		
 	}
 }
